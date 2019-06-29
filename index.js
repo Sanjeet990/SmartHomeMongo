b266a5992c41dfae5b748e758371e32ed9bfe5e8
Sanjeet Pathak <sanjeet.pathak990@gmail.com>
2019-06-29 20:28:51 +0530

    Changes

--- index.js
+++ index.js
@@ -146,31 +146,99 @@ function prepareDeviceData(userEmail){
 	})
 }
 
-app.onSync(async (body, headers) => {
-	const userEmail = await getEmail(headers);
-	//const userEmail = "sanjeet.pathak990@gmail.com";
-	var promisePrepare = prepareDeviceData(userEmail);
-	promisePrepare.then(function(devices){
-		var data = {
-			requestId: body.requestId,
-			payload: {
-			  agentUserId: userEmail,
-			  devices
-			}
-		  };
-		  console.log(data);
-		  return data;
-	}, function(error){
-		return {
-			requestId: body.requestId,
-			payload: {
-			  agentUserId: userEmail,
-			  devices: []
-			}
-		  };
-	});
-});
+// app.onSync(async (body, headers) => {
+// 	const userEmail = await getEmail(headers);
+// 	//const userEmail = "sanjeet.pathak990@gmail.com";
+// 	var promisePrepare = prepareDeviceData(userEmail);
+// 	promisePrepare.then(function(devices){
+// 		var data = {
+// 			requestId: body.requestId,
+// 			payload: {
+// 			  agentUserId: userEmail,
+// 			  devices
+// 			}
+// 		  };
+// 		  console.log(data);
+// 		  return data;
+// 	}, function(error){
+// 		return {
+// 			requestId: body.requestId,
+// 			payload: {
+// 			  agentUserId: userEmail,
+// 			  devices: []
+// 			}
+// 		  };
+// 	});
+// });
 
+app.onSync((body, headers) => {
+	// TODO Get devices for user
+	return {
+	  requestId: body.requestId,
+	  payload: {
+		agentUserId: "1836.15267389",
+		devices: [{
+		  id: "123",
+		  type: "action.devices.types.OUTLET",
+		  traits: [
+			"action.devices.traits.OnOff"
+		  ],
+		  name: {
+			defaultNames: ["My Outlet 1234"],
+			name: "Night light",
+			nicknames: ["wall plug"]
+		  },
+		  willReportState: false,
+		  roomHint: "kitchen",
+		  deviceInfo: {
+			manufacturer: "lights-out-inc",
+			model: "hs1234",
+			hwVersion: "3.2",
+			swVersion: "11.4"
+		  },
+		  otherDeviceIds: [{
+			deviceId: "local-device-id"
+		  }],
+		  customData: {
+			fooValue: 74,
+			barValue: true,
+			bazValue: "foo"
+		  }
+		}, {
+		  id: "456",
+		  type: "action.devices.types.LIGHT",
+		  traits: [
+			"action.devices.traits.OnOff",
+			"action.devices.traits.Brightness",
+			"action.devices.traits.ColorTemperature",
+			"action.devices.traits.ColorSpectrum"
+		  ],
+		  name: {
+			defaultNames: ["lights out inc. bulb A19 color hyperglow"],
+			name: "lamp1",
+			nicknames: ["reading lamp"]
+		  },
+		  willReportState: false,
+		  roomHint: "office",
+		  attributes: {
+			temperatureMinK: 2000,
+			temperatureMaxK: 6500
+		  },
+		  deviceInfo: {
+			manufacturer: "lights out inc.",
+			model: "hg11",
+			hwVersion: "1.2",
+			swVersion: "5.4"
+		  },
+		  customData: {
+			fooValue: 12,
+			barValue: false,
+			bazValue: "bar"
+		  }
+		}]
+	  }
+	};
+  });
 
 app.onQuery(async (body, headers) => {
   // TODO Get device state
